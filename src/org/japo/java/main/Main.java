3cd1f4c8edea8a6eb3c4722aa022f139db1c9456
mahiro21 <you@example.com>
2019-10-26 13:25:30 +0200

    :tada: commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,46 @@
+/* 
+ * Copyright 2019 Mario Merlos Abella <mario.merlos.alum@iescamp.es>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Mario Merlos Abella <mario.merlos.alum@iescamp.es>
+ */
+public final class Main {
+
+    public static void main(String[] args) {
+
+        // Variables
+        int canicasLeft;
+
+        //Constantes
+        final String Namecanica = "Ruperta";
+        final int CanicasIni = 5;
+        final int CanicasFin = 1;
+
+        System.out.printf("Secuencia de Canicas%n====================%n");
+        System.out.printf("Nombre de la canica ........: %s%n", Namecanica);
+          System.out.printf("---%n");
+        System.out.printf("Número de canicas inicial ..: %d%n", CanicasIni);
+        System.out.printf("Número de canicas final ....: %d%n", CanicasFin);
+        canicasLeft = CanicasIni - CanicasFin;
+        System.out.printf("---%n");
+        System.out.printf(Locale.ENGLISH, "Número de canicas perdidas .: %d%n", canicasLeft);
+
+    }
+}
